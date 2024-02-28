e90bc2a82d6f8a3bbe326df0fe62ea1559095100
hojin <jhj377@gmail.com>
2024-02-29 00:59:32 +0900

    [#14] 계정 정보에 대한 구조체 샘플

--- gcp/datastore/entity/sample/sample.go
+++ gcp/datastore/entity/sample/sample.go
@@ -4,3 +4,22 @@ type Test struct {
 	Key   string
 	Value string
 }
+
+type Account struct {
+	UUID          string // uuid
+	DeviceAppUUID string // device app uuid
+	CreatedAt     int64  // created at
+}
+
+type Platform struct {
+	UUID     string // Account uuid
+	Token    string // platform token
+	Platform string // platform name (ex. github, google, kakao)
+}
+
+type Profile struct {
+	UUID   string // Account uuid
+	Name   string // name
+	Desc   string // description
+	ImgURL string // img url
+}
