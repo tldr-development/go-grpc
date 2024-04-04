f261b37d9601264cbf9d5e95c0b2ec4f66fa4f0d
hojin <jhj377@gmail.com>
2024-04-04 23:39:19 +0900

    inspire after 생성 기준 변경

--- inspire/inspire.go
+++ inspire/inspire.go
@@ -98,7 +98,7 @@ func (s *server) GenerateInspireAfterCreatedLast(_ context.Context, request *pro
 	dbClient := datastore.GetClient(context.Background())
 	kind := datastore.GetKindByPrefix(app+env, "inspire_last")
 
-	query := datastore.NewQuery(kind).FilterField("Created", ">", request.GetCreated())
+	query := datastore.NewQuery(kind).FilterField("Created", "<", request.GetCreated())
 	inspires := []inspire_struct.Inspire{}
 	dbClient.GetAll(context.Background(), query, &inspires)
 
